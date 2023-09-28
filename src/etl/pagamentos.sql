68f7b48b933ce1a94c618a169a1b0057835c8b77
Francisco Santos <francismelo.jr@gmail.com>
2023-09-28 16:56:10 -0300

    initial commit

---
+++ src/etl/pagamentos.sql
@@ -0,0 +1,139 @@
+WITH tb_pedidos AS(
+SELECT 
+    DISTINCT
+    t1.idPedido,
+    t2.idVendedor
+
+FROM pedido AS t1
+
+LEFT JOIN item_pedido as t2
+on t1.idPedido = t2.idPedido
+
+WHERE t1.dtPedido < '2018-01-01'
+AND t1.dtPedido >= '2017-06-01'
+AND idVendedor IS NOT NULL
+),
+
+tb_join as (
+
+SELECT t1.idVendedor,
+        t2.*
+
+FROM tb_pedidos as t1
+
+LEFT JOIN pagamento_pedido as t2
+
+ON t1.idPedido = t2.idPedido
+),
+
+tb_group AS (
+
+SELECT idVendedor, descTipoPagamento,
+COUNT(DISTINCT idPedido) as qtdTipoPagamento,
+SUM(vlPagamento) as vlTipoPagamento
+
+FROM tb_join
+GROUP BY idVendedor, descTipoPagamento
+),
+
+tb_summary as
+
+(SELECT idVendedor,
+
+sum(CASE WHEN descTipoPagamento = 'credit_card' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) as qtde_credit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'boleto' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) as qtde_boleto_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'debit_card' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) as qtde_debit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'voucher' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) as qtde_voucher_pedido,
+
+------------------------------------------------------------------------------------
+
+sum(CASE WHEN descTipoPagamento = 'credit_card' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) as valor_credit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'boleto' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) as valor_boleto_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'debit_card' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) as valor_debit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'voucher' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) as valor_voucher_pedido,
+
+------------------------------------------------------------------------------------
+
+sum(CASE WHEN descTipoPagamento = 'credit_card' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) * 1.0 / sum(qtdTipoPagamento) as pct_qtd_credit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'boleto' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) * 1.0 / sum(qtdTipoPagamento) as pct_qtd_boleto_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'debit_card' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) * 1.0 / sum(qtdTipoPagamento) as pct_qtd_debit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'voucher' 
+        THEN qtdTipoPagamento 
+        ELSE 0 end) * 1.0 / sum(qtdTipoPagamento) as pct_qtd_voucher_pedido,
+
+------------------------------------------------------------------------------------
+
+sum(CASE WHEN descTipoPagamento = 'credit_card' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) / sum(vlTipoPagamento) as pct_valor_credit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'boleto' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) / sum(vlTipoPagamento) as pct_valor_boleto_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'debit_card' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) / sum(vlTipoPagamento) as pct_valor_debit_card_pedido,
+
+sum(CASE WHEN descTipoPagamento = 'voucher' 
+        THEN vlTipoPagamento 
+        ELSE 0 end) / sum(vlTipoPagamento) as pct_valor_voucher_pedido
+
+from tb_group
+
+GROUP BY idVendedor),
+
+tb_cartao AS (
+SELECT
+        idVendedor,
+        AVG(nrParcelas) as avgQtdParcelas,
+        MAX(nrParcelas) as maxQtdParcelas,
+        MIN(nrParcelas) as minQtdParcelas
+        FROM tb_join
+        WHERE descTipoPagamento = "credit_card"
+        GROUP BY idVendedor
+)
+
+SELECT 
+        '2018-01-01' AS dtReference,
+        t1.*,
+        t2.avgQtdParcelas,
+        t2.maxQtdParcelas,
+        t2.minQtdParcelas
+
+ FROM tb_summary as t1
+
+LEFT JOIN tb_cartao as t2
+
+ON t1.idVendedor = t2.idVendedor
---
+++ src/etl/query.sql
@@ -0,0 +1,5 @@
+SELECT *
+
+FROM item_pedido
+
+WHERE idPedido = "98fe01bc254b759c1f2c11d77c2891d5"
